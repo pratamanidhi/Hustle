15b422f2d51cfea64f4b907f76f4e2683e4dc4e4
Jalanidhi Pratama <jalanidhi.pratama@m2.formulatrix.com>
2025-05-19 21:42:39 +0700

    feat: add warehouse model

---
+++ HustleDatabase/Model/Warehouse/CoffeeModel.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class ClassCoffeeModel:
+    guid: str
+    name: str
+    description: str
+    stockInt: int
+    stockOut: int
+    totalStock: int
+    lastInput: datetime
+    lastOutput: datetime
+    updatedBy: str
+    price: int
+
