ea3d87d2a0a5d2a18ffd447a711a4a23934fd18c
Jalanidhi Pratama <jalanidhi.pratama@m2.formulatrix.com>
2025-05-21 22:25:24 +0700

    feat: add and update stock function

--- HustleBussiness/Warehouse/CoffeeBussiness.py
+++ HustleBussiness/Warehouse/CoffeeBussiness.py
@@ -12,4 +12,13 @@ class CoffeeBusiness():
         stock = Repo.GetStock(self, dbContext.Coffee, Coffee)
         return stock
 
+    def AddStock(self, coffee):
+        result = Repo.AddStock(self, dbContext.Coffee, coffee)
+        return result
+
+    def UpdateStock(self, isOut, coffee):
+        result = Repo.StockUpdate(self, dbContext.Coffee, isOut, coffee)
+        return result
+
+
 
--- HustleUserInterface/Main.py
+++ HustleUserInterface/Main.py
@@ -1,5 +1,6 @@
 from nicegui import ui
 import UserManagement.Login
+import Warehouse.MainContent
 
 if __name__ in {"__main__", "__mp_main__"}:
     ui.run(port=8081)
